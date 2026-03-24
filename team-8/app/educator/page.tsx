300d4bc4fdb3567742f9211e4ad60e8f776b7f7d
25LP8016 25LP8016 <25LP8016@G305-CC0243.local>
2026-03-24 15:15:18 +0800

    fix

---
+++ team-8/app/educator/page.tsx
@@ -0,0 +1,7 @@
+export default function EducatorPage() {
+  return (
+    <div>
+      <h1>Educator Page</h1>
+    </div>
+  );
+}
---
+++ team-8/app/student/page.tsx
@@ -0,0 +1,7 @@
+export default function StudentPage() {
+  return (
+    <div>
+      <h1>Student Page</h1>
+    </div>
+  );
+}
